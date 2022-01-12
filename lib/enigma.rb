044143b5951fd841899f84010984a5733a551431
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-12 13:10:18 -0500

    Feat: Can create #final_shift hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,7 +10,7 @@ class Enigma
     numbers.sample(5).join
   end
 
-  def split(key)
+  def split_key(key)
     array = []
     array << key[0..1].to_i
     array << key[1..2].to_i
@@ -23,5 +23,15 @@ class Enigma
     date_squared = date.to_i ** 2
     number_array = date_squared.to_s.chars
     number_array.last(4).join
+  end
+
+  def final_shift(key, offset)
+    offset_array = offset.chars
+    shift_hash = {
+      A: key[0] + offset[0].to_i,
+      B: key[1] + offset[1].to_i,
+      C: key[2] + offset[2].to_i,
+      D: key[3] + offset[3].to_i,
+    }
   end
 end
