1157c6e9ba2640e27989211275524c39a11a8b02
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-12 12:25:42 -0500

    Test: Changes date to offset number

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,13 +1,12 @@
 require 'date'
 require './lib/enigma'
 
-
 RSpec.describe Enigma do
   subject { Enigma.new }
 
-  it "exists" do
-    expect(subject).to be_a Enigma
-  end
+    it "exists" do
+      expect(subject).to be_a Enigma
+    end
 
   context "Finding the Shifts" do
 
@@ -16,9 +15,11 @@ RSpec.describe Enigma do
     end
 
     it "splits random five digit number" do
-
       expect(subject.split("02715")).to eq([02,27,71,15])
     end
-    
+
+    it "changes date to offset" do
+      expect(subject.date_to_offset("040895")).to eq("1025")
+    end
   end
 end
