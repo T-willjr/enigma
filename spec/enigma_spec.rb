ef18734856bfbd132f06a14a000445b7ba3cf6cc
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-12 13:09:06 -0500

    Test: Creates final shift hash

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -15,11 +15,27 @@ RSpec.describe Enigma do
     end
 
     it "splits random five digit number" do
-      expect(subject.split("02715")).to eq([02,27,71,15])
+      expect(subject.split_key("02715")).to eq([02,27,71,15])
     end
 
     it "changes date to offset" do
       expect(subject.date_to_offset("040895")).to eq("1025")
     end
+  end
+
+  context "Encrypting a message" do
+
+    it "creates final shift" do
+      key = [02,27,71,15]
+      offset = "1025"
+      final_shift = {
+        A: 3,
+        B: 27,
+        C: 73,
+        D: 20
+      }
+      expect(subject.final_shift(key, offset)).to eq(final_shift)
+    end
+
   end
 end
