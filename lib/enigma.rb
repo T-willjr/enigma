f334e6d05128943fc1adf93d5a75c2b316b3b354
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-12 12:16:27 -0500

    Feat: #key_generator

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,8 +2,12 @@ require 'pry'
 class Enigma
 
   def initialize
-    @numbers = (0..9).to_a
     @alphabet = ("a".."z").to_a << " "
+  end
+
+  def key_generator
+    numbers = (0..9).to_a
+    numbers.sample(5).join
   end
 
 end
