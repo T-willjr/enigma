92a1cd4a505f6b1b93c02944b7970f698ccf3958
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-14 13:29:42 -0500

    Feat: Encrpyts message in txt & returns encryption to encrypted.txt

---
+++ lib/encrypt.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require_relative('enigma')
+
+enigma = Enigma.new
+
+message_txt = ARGV.first
+encrypt_txt = ARGV[1]
+message = File.read(message_txt).strip
+enigma.encrypt(message, "02715", "040895")
+encrypted_file = File.open(encrypt_txt, "w+") { |file| file.write(enigma.encrypted_message) }
+
+puts "Created '#{encrypt_txt}' with the key #{enigma.key} and date #{enigma.date}"
