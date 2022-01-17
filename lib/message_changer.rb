96122c275967e499b4953e1c3b4e289d68e27a67
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-17 10:14:42 -0500

    Refactor: Removed repeated lines. Removed #encrpyt/#decrypt to enigma.rb

--- lib/message_changer.rb
+++ lib/message_changer.rb
@@ -34,18 +34,15 @@ module MessageChanger
 
   def encrypted_letter(letter)
     @shift_count += 1
+    letter_index = @alphabet.index(letter)
     if @shift_count == 1
-      letter_index = @alphabet.index(letter)
       letter_encrypted = @alphabet.rotate(letter_index + @shift_hash[:A])[0]
     elsif @shift_count == 2
-      letter_index = @alphabet.index(letter)
       letter_encrypted = @alphabet.rotate(letter_index + @shift_hash[:B])[0]
     elsif @shift_count == 3
-      letter_index = @alphabet.index(letter)
       letter_encrypted = @alphabet.rotate(letter_index + @shift_hash[:C])[0]
     elsif @shift_count == 4
       @shift_count = 0
-      letter_index = @alphabet.index(letter)
       letter_encrypted = @alphabet.rotate(letter_index + @shift_hash[:D])[0]
     end
     letter_encrypted
@@ -53,41 +50,18 @@ module MessageChanger
 
   def decrypted_letter(letter)
     @shift_count += 1
+    letter_index = @alphabet.index(letter)
     if @shift_count == 1
-      letter_index = @alphabet.index(letter)
       letter_decrypted = @alphabet.rotate(letter_index - @shift_hash[:A])[0]
     elsif @shift_count == 2
-      letter_index = @alphabet.index(letter)
       letter_decrypted = @alphabet.rotate(letter_index - @shift_hash[:B])[0]
     elsif @shift_count == 3
-      letter_index = @alphabet.index(letter)
       letter_decrypted = @alphabet.rotate(letter_index - @shift_hash[:C])[0]
     elsif @shift_count == 4
       @shift_count = 0
-      letter_index = @alphabet.index(letter)
       letter_decrypted = @alphabet.rotate(letter_index - @shift_hash[:D])[0]
     end
     letter_decrypted
   end
 
-  def encrypt(message, key =key_generator, date =Date.today.strftime("%d%m%y"))
-    @key = key
-    @date = date
-    @encrypted = {
-      encryption: message_generator(message, key, date),
-      key: key,
-      date: date
-    }
-  end
-
-  def decrypt(message, key, date =Date.today.strftime("%d%m%y"))
-    @key = key
-    @date = date
-    @message_to_be_encrypted = false
-    {
-      decryption: message_generator(message, key, date),
-      key: key,
-      date: date
-    }
-  end
 end
