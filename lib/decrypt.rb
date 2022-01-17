3687fcaf5e5da2fd2e85c6728f60f6fb0706fab8
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-17 11:49:38 -0500

    Refactor: Update encrypted_message to new_message

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,5 +1,4 @@
 require_relative('enigma')
-
 enigma = Enigma.new
 
 encrypted_txt = ARGV.first
@@ -8,6 +7,6 @@ decrypt_key = ARGV[2]
 decrypt_date = ARGV[3]
 encrypted_message = File.read(encrypted_txt).strip
 enigma.decrypt(encrypted_message, decrypt_key, decrypt_date)
-encrypted_file = File.open(decrypt_txt, "w+") { |file| file.write(enigma.decrypted_message) }
+encrypted_file = File.open(decrypt_txt, "w+") { |file| file.write(enigma.new_message) }
 
 puts "Created '#{decrypt_txt}' with the key #{enigma.key} and date #{enigma.date}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,11 +1,10 @@
 require_relative('enigma')
-
 enigma = Enigma.new
 
 message_txt = ARGV.first
 encrypt_txt = ARGV[1]
 message = File.read(message_txt).strip
 enigma.encrypt(message, "02715", "040895")
-encrypted_file = File.open(encrypt_txt, "w+") { |file| file.write(enigma.encrypted_message) }
+encrypted_file = File.open(encrypt_txt, "w+") { |file| file.write(enigma.new_message) }
 
 puts "Created '#{encrypt_txt}' with the key #{enigma.key} and date #{enigma.date}"
